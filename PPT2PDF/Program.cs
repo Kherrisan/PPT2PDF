b7f4b96253b1fabf3a67437f6ca224d5e8c3a92a
Dokyme <zdkscope@qq.com>
2018-02-03 15:36:32 +0800

    Remove .idea

---
+++ PPT2PDF/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PPT2PDF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
